26200679e7eccca09ed77f9defc4c5dd376efa34
tiagological <t.marques999@gmail.com>
2020-09-06 17:47:53 +0100

    Stage 12 completed

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -3,8 +3,7 @@ import { body } from 'express-validator';
 import jwt from 'jsonwebtoken';
 import { Password } from '../services/password';
 import { User } from '../models/user';
-import { validateRequest } from '../middlewares/validate-request';
-import { BadRequestError } from '../errors/bad-request-error';
+import { validateRequest, BadRequestError } from '@tiagological-tickets/common';
 
 const router = express.Router();
 
