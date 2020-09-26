8ca87252cc392f4549612c58f3194144b6693a90
tiagological <t.marques999@gmail.com>
2020-09-26 16:37:28 +0100

    Up to section 13- lesson 245

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -5,7 +5,7 @@ export default ({ req }) => {
     return axios.create({
       // Does not match url used in course, see below link for more info
       // https://www.udemy.com/course/microservices-with-node-js-and-react/learn/lecture/19122264#questions/10323992
-      baseURL: 'http://172.17.0.2',
+      baseURL: 'http://172.17.0.4',
       headers: req.headers,
     });
   } else {
